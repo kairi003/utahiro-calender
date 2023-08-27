9e855ab66211b4e3000c3defbcb6b80dfa0a3935
kairi003 <kairi.satellite@gmail.com>
2023-08-27 13:41:03 +0900

    add

---
+++ README.md
@@ -0,0 +1,6 @@
+# utahiro-calender
+ウタヒロ室料半額DAYカレンダー更新用スクリプト
+
+- [Task Runner](https://github.com/kairi003/utahiro-shedule/)
+- [ウタヒロ室料半額DAYカレンダー](https://calendar.google.com/calendar/u/0?cid=MTExNTZkNzVmMmQyZjA3ZTIwYWRjMTMxZmZjOGRkNWQ0NjdlZGRhOTExNGE2YzI4ZjM1N2IwM2UxNjU3OGNhMkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t)
+- [Google Apps Script](https://script.google.com/d/1fkLfm6z0P-uNt8rLwgwpKooO1DPHAGkQMzXqMccMsvTV6zjNjrnBUZha) ([Source](https://github.com/kairi003/utahiro-calender))
---
+++ dist/main.js
@@ -0,0 +1,45 @@
+// echo -n ${SECRET_KEY} | sha256sum
+
+function doPost(e) {
+  if (!e) throw new Error(`Invalid arguments`);
+
+  const props = PropertiesService.getScriptProperties();
+  const KEY_HASH = props.getProperty('KEY_HASH');
+  
+  try {
+    const data = JSON.parse(e.postData.contents);
+    const {key, content: {title, date}} = data;
+    if (hash(key) !== KEY_HASH) throw new Error(`Invalid key`);
+    const message = addEventIfNotExists(title, date);
+    const output = ContentService.createTextOutput();
+    output.setMimeType(ContentService.MimeType.JSON);
+    output.setContent(JSON.stringify({ status: "success", message}));
+    return output;
+  } catch(err) {
+    const output = ContentService.createTextOutput();
+    output.setMimeType(ContentService.MimeType.JSON);
+    output.setContent(JSON.stringify({ status: "failed", message: err.toString()}));
+    return output;
+  }
+}
+
+function hash(key) {
+  const bytes = Utilities.computeDigest(Utilities.DigestAlgorithm.SHA_256, key);
+  const hexHash = bytes.map(b=>(b&0xFF).toString(16).padStart(2,0)).join('');
+  return hexHash;
+}
+
+function addEventIfNotExists(title, dateSrc) {
+  if (!title || !dateSrc) throw new Error(`Invalid arguments`);
+
+  const props = PropertiesService.getScriptProperties();
+  const CALENDER_ID = props.getProperty('CALENDER_ID');
+  const calendar = CalendarApp.getCalendarById(CALENDER_ID);
+  const date = new Date(dateSrc);
+  const existings = calendar.getEventsForDay(date);
+  const duplicates = existings.filter(e => e.getTitle() === title);
+  if (duplicates.length > 0) return 'DUPLICATE_EVENT';
+  calendar.createAllDayEvent(title, date);
+  console.log(`create ${date.toLocaleDateString()} event`);
+  return 'CREATE_EVENT';
+}
